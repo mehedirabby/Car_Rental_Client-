f6bc408acda326680f0a5dd2e369a97be8111adb
Mehedi rabby <mehedi15-2995@diu.edu.bd>
2022-11-08 21:08:01 +0600

    service data added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,60 +3,41 @@ import Banner1 from "../../../assets/homeCarousel/Banner1.jpg";
 import Banner8 from "../../../assets/homeCarousel/Banner8.jpg";
 import banner2 from "../../../assets/homeCarousel/banner2.jpg";
 import banner3 from "../../../assets/homeCarousel/banner3.jpg";
+import "./Banner.css";
+import BannerItems from "./BannerItems";
 
 const Banner = () => {
+  const bannerData = [
+    {
+      image: Banner1,
+      prev: 8,
+      id: 1,
+      next: 2,
+    },
+    {
+      image: banner2,
+      prev: 1,
+      id: 2,
+      next: 3,
+    },
+    {
+      image: banner3,
+      prev: 2,
+      id: 3,
+      next: 8,
+    },
+    {
+      image: Banner8,
+      prev: 3,
+      id: 8,
+      next: 5,
+    },
+  ];
   return (
-    <div className="carousel w-full">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img src={Banner1} className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <h3 className="text-5xl text-white">
-            best Rental service <br />
-            In the dhaka
-          </h3>
-        </div>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide2" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img src={banner2} className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img src={banner3} className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide4" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <img src={Banner8} className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide1" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
+    <div className="carousel w-full py-10">
+      {bannerData.map((slide) => (
+        <BannerItems key={slide.id} slide={slide}></BannerItems>
+      ))}
     </div>
   );
 };
---
+++ src/Pages/Home/Banner/BannerItems.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const BannerItems = ({ slide }) => {
+  const { image, id, prev, next } = slide;
+  return (
+    <div id={`slide${id}`} className="carousel-item relative w-full">
+      <div className="carousel-img">
+        <img src={image} className="w-full" />
+      </div>
+      <div className="absolute flex justify-between transform -translate-y-1/2  left-24  top-1/4">
+        <h3 className="text-5xl text-white font-bold">
+          Best Car Rental <br /> service <br />
+          In Dhaka
+        </h3>
+      </div>
+      <div className="absolute flex justify-between transform -translate-y-1/2 left-24 text-xl w-2/5  top-1/2">
+        <p className="text-white">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+          vitae fugiat accusamus eum quod fuga? Mollitia eveniet error officiis
+          dicta, perferendis velit maiores porro cum aliquid, illum repudiandae
+          nobis. Amet.
+        </p>
+      </div>
+      <div className="absolute flex justify-start transform -translate-y-1/2 w-2/5 left-24 top-3/4">
+        <button className="btn btn-warning mr-5">Warning</button>
+        <button className="btn btn-outline btn-error">Warning</button>
+      </div>
+
+      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href={`#slide${prev}`} className="btn btn-circle">
+          ❮
+        </a>
+        <a href={`#slide${next}`} className="btn btn-circle">
+          ❯
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default BannerItems;
