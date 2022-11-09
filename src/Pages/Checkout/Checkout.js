51b377c542c30d92fd462264676db32675a99109
Mehedi rabby <mehedi15-2995@diu.edu.bd>
2022-11-10 00:36:27 +0600

    checked again again

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import "../../App.css";
+import { AuthContext } from "../../Context/AuthProvider/AuthProvider";
 
 const Checkout = () => {
-  const { title, img, price, description } = useLoaderData();
+  const { title, _id, img, price, description } = useLoaderData();
+  const { user } = useContext(AuthContext);
   return (
     <section>
       <div>
@@ -21,30 +24,53 @@ const Checkout = () => {
 
         {/* another section */}
 
-        <div>
+        <div className="pt-8">
+          <h2 className="text-3xl App text-orange-800 pb-6">
+            Review for {title}
+          </h2>
           <form>
-            <div className="grid grid-cols-1 lg:grid-cols-2">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
               <input
                 type="text"
-                placeholder="Type here"
-                className="input input-bordered w-full max-w-xs"
+                placeholder="first name"
+                className="input input-bordered w-full "
+                name="firstName"
+                required
               />
               <input
                 type="text"
-                placeholder="Type here"
-                className="input input-bordered w-full max-w-xs"
+                placeholder="last name"
+                className="input input-bordered w-full "
+                name="lastName"
+                required
               />
               <input
                 type="text"
-                placeholder="Type here"
-                className="input input-bordered w-full max-w-xs"
+                placeholder="your phone"
+                className="input input-bordered w-full "
+                name="phone"
+                required
               />
               <input
-                type="text"
-                placeholder="Type here"
-                className="input input-bordered w-full max-w-xs"
+                type="email"
+                defaultValue={user?.email}
+                placeholder="your email"
+                className="input input-bordered w-full "
+                name="email"
+                readOnly
+                required
               />
+              <textarea
+                className="textarea textarea-bordered h-24 w-full"
+                placeholder="Write Your Review Here"
+                name="message"
+              ></textarea>
             </div>
+            <input
+              className="App btn btn-primary mt-2"
+              type="submit"
+              value="Place Your Review"
+            />
           </form>
         </div>
       </div>
